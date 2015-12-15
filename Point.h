be77ab9b81caeb262e770279aacb6929b41677fb
cloerj <john-marc.cloer@ucdenver.edu>
2015-12-14 17:13:29 -0700

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,46 +1,49 @@
 //
-// Point Header file provided to us by Professor Ivo
+// John-Marc Cloer
 //
 
-#ifndef CLUSTERING_POINT_H
-#define CLUSTERING_POINT_H
+#ifndef PA2_POINT_H
+#define PA2_POINT_H
 
 #include <iostream>
 
 namespace Clustering {
 
     class Point {
-        int dim;        // number of dimensions of the point
-        double *values; // values of the point's dimensions
+
+    private:                    // Declaring Private as a reminder
+        int dim;                // number of dimensions of the point
+        double *values;         // values of the point's dimensions
 
     public:
+        // Constructors
         Point(int);
         Point(int, double *);
 
-        // Big three: cpy ctor, overloaded operator=, dtor
-        Point(const Point &);
-        Point &operator=(const Point &);
-        ~Point();
+        // Big Three:
+        Point(const Point &);                   //Copy Constructor
+        Point &operator=(const Point &);        //Assignment operator
+        ~Point();                               //Destructor
 
-        // Accessors & mutators
+        // Accessors & Mutators
         int getDims() const { return dim; }
         void setValue(int, double);
         double getValue(int) const;
+        void printPoint();
 
         // Functions
         double distanceTo(const Point &) const;
 
-        // Overloaded operators
-
-        // Members
+        //Members
         Point &operator*=(double);
         Point &operator/=(double);
-        const Point operator*(double) const; // prevent (p1*2) = p2;
+        const Point operator*(double) const;
         const Point operator/(double) const;
 
-        double &operator[](int index) { return values[index - 1]; } // TODO out-of-bds?
+        //1-based index
+        double& operator[](int index);
 
-        // Friends
+        // Overloaded operators
         friend Point &operator+=(Point &, const Point &);
         friend Point &operator-=(Point &, const Point &);
         friend const Point operator+(const Point &, const Point &);
@@ -57,6 +60,7 @@ namespace Clustering {
         friend std::ostream &operator<<(std::ostream &, const Point &);
         friend std::istream &operator>>(std::istream &, Point &);
     };
-
 }
-#endif //CLUSTERING_POINT_H
+
+
+#endif //PA2_POINT_H
